02493630a0b84391864cd117f2fea3694dc90061
brayan david orrego <brayan.orrego@gmail.com>
2025-12-16 18:28:34 -0500

    Primer commit del aa5 ev01

---
+++ admin.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Pagina inicial</title>
+</head>
+<body>
+	<div class="contenedor_mensaje">
+		<p class="mensaje">
+			bienvenido!
+			<a href="index.php">volver</a>
+		</p>
+	</div>
+	
+</body>
+</html>
